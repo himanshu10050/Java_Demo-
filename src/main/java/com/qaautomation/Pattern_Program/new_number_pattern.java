b3457298107bc814ace0b9a0ca2b09efa4e4ad90
himanshu10050 <hb047809@gmail.com>
2024-01-12 16:09:46 +0530

    12-jan

--- src/main/java/com/qaautomation/Pattern_Program/new_number_pattern.java
+++ src/main/java/com/qaautomation/Pattern_Program/new_number_pattern.java
@@ -2,28 +2,16 @@ package com.qaautomation.Pattern_Program;
 
 public class new_number_pattern {
     public static void main(String[] args) {
-        // int n = 5;
-        // for (int i = 1; i <= n; i++) {
 
-        // for (int j = i; j <= i; j++) {
-        // System.out.print(i);
-
-        // // for (int k = i; k <= i; k++) {
-        // // System.out.print(i);
-        // // }
-        // }
-        // System.out.println();
-
-        // }
-        // }
-        // }
         int n = 4;
-        int k = 1;
+        int m = 1;
         for (int i = 1; i <= n; i++) {
-            // System.out.println("*");
 
             for (int j = 1; j <= i; j++) {
-                System.err.print(k++);
+                System.out.print(i);
+                // for (int k = 1; k <= i; k++) {
+                // System.out.print(j);
+                // }
             }
             System.out.println(" ");
         }
