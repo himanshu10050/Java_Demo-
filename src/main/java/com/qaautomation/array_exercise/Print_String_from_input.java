2f40ae07aeb5a2abacd0f93484938d5064ef43df
himanshu10050 <hb047809@gmail.com>
2024-01-16 14:20:03 +0530

    16-jan

---
+++ src/main/java/com/qaautomation/array_exercise/Print_String_from_input.java
@@ -0,0 +1,28 @@
+package com.qaautomation.array_exercise;
+
+import java.util.Scanner;
+
+public class Print_String_from_input {
+
+    public static void main(String[] args) {
+        // Enter the size
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter size of array");
+        int size = sc.nextInt();
+
+        // Store int size in the String array
+        System.out.println("enter names : ");
+        String names[] = new String[size];
+
+        // Input
+        for (int i = 0; i < names.length; i++) {
+            names[i] = sc.next();
+        }
+
+        // Output
+        for (int i = 0; i < names.length; i++) {
+            System.out.println("enter the printed names : " + names[i]);
+        }
+
+    }
+}
